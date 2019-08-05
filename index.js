d09256712d1e11dc8b3a9b3a48a9f53f84c85cda
Simon Renlund <renlund.simon@gmail.com>
2019-08-05 15:58:47 +0300

    Fram till 3.8

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
-
 app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const morgan = require('morgan')
+app.use(morgan(function (tokens, req, res) {
+  let log = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+  if (req.method === 'POST') {
+    log = log.concat(JSON.stringify(req.body))
+  }
+  return log.join(' ')
+}))
 
 let notes = [
   {
@@ -82,16 +97,52 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-app.get('api/persons/:id', (req, res) => {
-  /*const id = Number(req.params.id)
-  const person = persons.filter(person => person.id !== id)
-  res.send('<div>'+id+'</id>')
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.filter(person => person.id === id)
   if (person) {
     res.json(person)
   } else {
-    //res.status(404).end()
-  }*/
+    res.status(404).end()
+  }
 })
+
+app.post('/api/persons', (req, res) => {
+  let error = 0
+  if (!req.body.name || !req.body.number) {
+    error = 1
+    res.status(404).json({
+      error: 'content missing'
+    })
+  }
+  for (let i=0;i<persons.length;i++) {
+    if (persons[i].name === req.body.name) {
+      error = 1
+      res.status(400).json({
+        error: 'name must be unique'
+      })
+    }
+  }
+  //if (error === 0) {
+    const id = Math.floor(Math.random()*10000)
+    const person = {
+      id: id,
+      name: req.body.name,
+      number: req.body.number
+    }
+    persons = persons.concat(person)
+    res.json(person)
+    //res.send('<div>'+ person.name + ' with number ' + person.number + ' was added successfully!')
+  //}
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 
 app.get('/info', (req,res) => {
   const d = new Date()
@@ -102,7 +153,6 @@ app.get('/info', (req,res) => {
     '<div>Phonebook has info for '+ amount +' people.</div><div>'+ d + '</div>'
   )
 })
-
 
 
 const PORT = 3001
