ee5db3d28454cc0a1f105305b4abe060be911aae
Simon Renlund <renlund.simon@gmail.com>
2019-08-09 11:50:02 +0300

    made the handler send the error message as json

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'CastError' && err.kind == 'ObjectId') {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (err.name === 'ValidationError') {
-    return res.status(400).send(err.message)
+    return res.status(400).json({error: err.message})
   }
   next(err)
 }
